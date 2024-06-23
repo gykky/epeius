0a2401299d62d1d4a29686c4495638b775b7a7e3
gykky <1697511386@qq.com>
2024-06-23 14:06:10 +0800

    _worker.js

--- _worker.js
+++ _worker.js
@@ -1,6 +1,6 @@
 // worker.src.js
 import { connect } from "cloudflare:sockets";
-let password = 'auto';
+let password = 'dz22456';
 let proxyIP = '';
 // The user name and password do not contain special characters
 // Setting the address will ignore proxyIP
